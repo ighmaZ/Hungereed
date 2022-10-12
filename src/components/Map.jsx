28654400de9ba29758e9ea843fc958f721924c01
ighmaz <ighmazcool@gmail.com>
2022-10-13 01:04:03 +0530

    fixed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -29,12 +29,16 @@ return (
 <h1 className='text-3xl'>{mapItem.title}</h1>
 
 {/* iframe here is used to show map */}
-<Iframe  width="600" height="450" src="https://datastudio.google.com/embed/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params=%7B%22ds2.name2%22:%22Subway%22%7D" ></Iframe>
+<Iframe  width="600" height="450" url="https://datastudio.google.com/embed/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params=%7B%22ds2.name2%22:%22Subway%22%7D"></Iframe>
 </div>
 
 <div className='flex flex-row items-center justify-center mt-8 w-full mb-10'>
  <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-32'>Bookmark</button>
- <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Remove</button>
+ <button 
+ onClick={()=>{
+   removeItem(mapItem.id)
+ }}
+ className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Remove</button>
  </div>   
 </div>
  
